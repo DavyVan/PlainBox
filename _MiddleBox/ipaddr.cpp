1d46de0511ea0e3a9456a0e17db7ac180c7cc8f2
DavyVan <affqqa@163.com>
2015-05-03 00:04:55 -0700

    ipaddr.cpp done.

--- _MiddleBox/ipaddr.cpp
+++ _MiddleBox/ipaddr.cpp
@@ -1,4 +1,5 @@
 #include "ipaddr.h"
+#include<arpa/inet.h>
 
 IPAddr::IPAddr()
 {
@@ -8,4 +9,61 @@ IPAddr::IPAddr()
 IPAddr::~IPAddr()
 {
     //dtor
+}
+
+IPv4Addr::IPv4Addr()
+    : ip_(0)
+{
+
+}
+
+IPv4Addr::IPv4Addr(uint32_t ip)
+    : ip_(ip)
+{
+
+}
+
+IPv4Addr::getAddr_raw()
+{
+    return &ip_;
+}
+
+IPv4Addr::getAddr_str()
+{
+    char buf[INET_ADDRSTRLEN + 1] = {0};
+    inet_ntop(AF_INET, &ip_, buf, INET_ADDRSTRLEN);
+    return buf;
+}
+
+ostream& operator<< (ostream& os, const IPv4Addr &ip)
+{
+    return os << ip.getAddr_str();
+}
+
+IPv6Addr::IPv6Addr()
+{
+    memset(ip_, 0, 16);
+}
+
+IPv6Addr::IPv6Addr(uint8_t* ip)
+{
+    memcpy(ip_, ip, 16);
+}
+
+IPv6Addr::getAddr_raw()
+{
+    return ip_;
+}
+
+IPv6Addr::getAddr_str()
+{
+    char buf[INET6_ADDRSTRLEN + 1] = {0};
+    inet_ntop(AF_INET6, &ip_, buf, INET6_ADDRSTRLEN);
+    return buf;
+}
+
+
+ostream& operator<< (ostream& os, const IPv6Addr &ip)
+{
+    return os << ip.getAddr_str();
 }
