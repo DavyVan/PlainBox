522a30b57c755fe6eefa32870730d64664af507e
DavyVan <affqqa@163.com>
2015-07-22 11:33:40 +0800

    Create install.sh

---
+++ _MiddleBox/install.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+apt-get install bison
+apt-get install g++
+apt-get install make
+apt-get install flex
+wget http://www.tcpdump.org/release/libpcap-1.7.4.tar.gz
+tar -zxvf libpcap-1.7.4.tar.gz
+rm libpcap-1.7.4.tar.gz
+cd libpcap-1.7.4/
+./configure
+make
+make install
+cd ..
+wget http://ufpr.dl.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.gz
+tar -zxvf boost_1_58_0.tar.gz
+rm boost_1_58_0.tar.gz
+apt-get install libssl-dev
+wget http://www.netfilter.org/projects/libnfnetlink/files/libnfnetlink-1.0.1.tar.bz2
+tar --bzip2 -xf libnfnetlink-1.0.1.tar.bz2
+rm libnfnetlink-1.0.1.tar.bz2
+cd libnfnetlink-1.0.1/
+./configure
+make
+make install
+cd ..
+apt-get install pkg-config
+wget http://www.netfilter.org/projects/libmnl/files/libmnl-1.0.3.tar.bz2
+tar --bzip2 -xf libmnl-1.0.3.tar.bz2
+rm libmnl-1.0.3.tar.bz2
+cd libmnl-1.0.3/
+./configure
+make
+make install
+cd ..
+wget http://www.netfilter.org/projects/libnetfilter_queue/files/libnetfilter_queue-1.0.2.tar.bz2
+tar --bzip2 -xf libnetfilter_queue-1.0.2.tar.bz2
+rm libnetfilter_queue-1.0.2.tar.bz2
+cd libnetfilter_queue-1.0.2/
+./configure
+make
+make install
+cd ..
+apt-get install libglib2.0-dev
+wget https://ftp.gnu.org/gnu/gmp/gmp-6.0.0a.tar.bz2
+tar --bzip2 -xf gmp-6.0.0a.tar.bz2
+rm gmp-6.0.0a.tar.bz2
+cd gmp-6.0.0/
+./configure
+make
+make install
+cd ..
+wget http://crypto.stanford.edu/pbc/files/pbc-0.5.14.tar.gz
+tar -zxvf pbc-0.5.14.tar.gz
+rm pbc-0.5.14.tar.gz
+cd pbc-0.5.14/
+./configure
+make
+make install
+cd ..
+wget http://acsc.cs.utexas.edu/cpabe/libbswabe-0.9.tar.gz
+tar -zxvf libbswabe-0.9.tar.gz
+rm libbswabe-0.9.tar.gz
+cd libbswabe-0.9/
+./configure
+make
+make install
+cd ..
+apt-get install git
+git clone https://github.com/DavyVan/LearningLibpcap.git
